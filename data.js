b3ba9660b4518bbf0bb1f8b37daaaed109bf2a1a
hannegrijp <61147814+hannegrijp@users.noreply.github.com>
2020-02-28 22:27:40 +0100

    hanne grijp

--- data.js
+++ data.js
@@ -36,7 +36,7 @@ const data = {
             {
                 "text": "Adobe XD Prototype",
                 "title": "Mijn Adobe XD prototype (share link)",
-                "href": "https://xd.adobe.com/view/e4c35170-a2f7-4c10-753b-ba7c449703ec-92f2/grid",
+                "href": "https://xd.adobe.com/view/0169c342-ed22-4b92-4f16-ead5e13aee2c-7758/",
                 "target": "_blank"
             }
         ]
